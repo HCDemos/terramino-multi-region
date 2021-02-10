8d9ce4b693c1e2c211225ffa26e4f523ca77c88d
peada04 <peada04@yahoo.com>
2021-02-10 08:11:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,23 @@
 # main.tf
-module "us-east-2" {
-  source  = "app.terraform.io/HCDemos/terramino/aws"
-  version = "1.0.0"
+#module "us-east-2" {
+#  source  = "app.terraform.io/HCDemos/terramino/aws"
+#  version = "1.0.0"
   # insert required variables here
-  region = "us-east-2"
-  ami_id_value = "ami-0bbf2c4f0b9b24139"  
-}
+#  region = "us-east-2"
+#  ami_id_value = "ami-0bbf2c4f0b9b24139"  
+#}
   
-module "eu-west-1" {
-  source  = "app.terraform.io/HCDemos/terramino/aws"
-  version = "1.0.0"
-  region = "eu-west-1"
-  ami_id_value = "ami-035c845062830cd63"
-}
+#module "eu-west-1" {
+#  source  = "app.terraform.io/HCDemos/terramino/aws"
+#  version = "1.0.0"
+#  region = "eu-west-1"
+#  ami_id_value = "ami-035c845062830cd63"
+#}
 
   module "us-west-1" {
   source  = "app.terraform.io/HCDemos/terramino/aws"
   version = "1.0.0"
   region = "us-west-1"
   ami_id_value = "ami-03b5d9a16d1261864"
+  count = 5
 }
